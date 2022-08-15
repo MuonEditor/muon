b40904c24758f12257158456a2c76144bda5a7ca
benkyd <benjaminkyd@gmail.com>
2022-08-15 22:37:20 +0000

    changed the shaders around

--- res/font.glsl
+++ res/font.glsl
@@ -1,3 +1,17 @@
+#type vertex
+#version 330 core
+
+layout (location = 0) in vec4 vtx; // position and texture
+
+out vec2 fUV;
+
+void main() {
+    gl_Position = vec4(vtx.xy, 0.0, 1.0);
+    fUV = vtx.zw;
+}
+
+
+#type fragment
 #version 330 core
 
 in vec2 fUV;
