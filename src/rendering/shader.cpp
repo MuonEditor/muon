d3994447a0faf00398ca3a11e3c2dad57813381b
benkyd <benjaminkyd@gmail.com>
2022-08-15 22:37:00 +0000

    new shader pipeline

--- src/rendering/shader.cpp
+++ src/rendering/shader.cpp
@@ -2,75 +2,113 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
+
+static std::unordered_map<GLuint, int> Shader2Index = {
+	{GL_VERTEX_SHADER, 0},
+	{GL_GEOMETRY_SHADER, 1},
+	{GL_FRAGMENT_SHADER, 2}
+};
 
 using namespace muon::rendering;
 
-std::string textFromFile(const std::string& path) {
+std::string textFromFile(const std::filesystem::path& path) {
     std::ifstream input(path);
     return std::string((std::istreambuf_iterator<char>(input)), 
                         std::istreambuf_iterator<char>());
 
 }
 
-_Shader::_Shader() {
-	program = 0;
-	mFrag = 0;
-	mVert = 0;
+_Shader::_Shader()
+	: mShaders({ GL_NONE, GL_NONE, GL_NONE})
+	, mProgram(0) {
 }
 
-void _Shader::load(std::string path) {
-	std::string vertexLocation = path + ".vert";
-	load(vertexLocation, GL_VERTEX_SHADER);
 
-	std::string fragmentLocation = path + ".frag";
-	load(fragmentLocation, GL_FRAGMENT_SHADER);
+_Shader::~_Shader() {
+	for (int i = 0; i < 3; i++) {
+		if (mShaders[i] == GL_NONE) continue;
+		glDeleteShader(mShaders[i]);
+	}
 }
 
-void _Shader::load(std::string path, GLenum type) {
-	GLuint activeShader = 0;
+std::shared_ptr<_Shader> _Shader::load(std::filesystem::path path) {
 
-	if (type == GL_VERTEX_SHADER)
-		mVert = activeShader = glCreateShader(type);
-
-	if (type == GL_FRAGMENT_SHADER)
-		mFrag = activeShader = glCreateShader(type);
+	assert(std::filesystem::exists(path));
 
 	std::string loadedShaderSource = textFromFile(path);
-	const char* shaderSource = loadedShaderSource.c_str();
-	int shaderSourceLength = loadedShaderSource.length();
 
-	glShaderSource(activeShader, 1, &shaderSource, &shaderSourceLength);
+	for (int i = 0; i < loadedShaderSource.length(); i++) {
+		const char& c = loadedShaderSource[i];
+		if (c == '#') {
+			mPreprocessorDefinition def = { .start = i };
+			int j;
+			for (j = ++i; loadedShaderSource[j] != ' '; j++) {
+				def.key += loadedShaderSource[j];
+			}
+			for (j++; loadedShaderSource[j] != '\n'; j++) {
+				def.def += loadedShaderSource[j];
+			}
+			def.end = j; i = j; // advance i
+			mDefinitions.push_back(def);
+		}
+	}
+
+	// now we have all of the key/value definitions
+	// we can extract the relavent ones, for example
+	// "type"
+	auto types = mGetKeys("type");
+	int i = 0;
+	for (const mPreprocessorDefinition* type : types) {
+		GLuint glType = GL_NONE;
+		if (type->def == "vertex") glType = GL_VERTEX_SHADER;
+		if (type->def == "geometry") glType = GL_GEOMETRY_SHADER;
+		if (type->def == "fragment") glType = GL_FRAGMENT_SHADER;
+		assert(glType != GL_NONE);
+
+		mShaders[Shader2Index[glType]] = glCreateShader(glType);
+		
+		const char* source = loadedShaderSource.c_str() + type->end;
+		int end = types.size() - 1 == i ? types.size(): types[i + 1]->start;
+		int length = end - type->end;
+		
+		glShaderSource(mShaders[Shader2Index[glType]], 1, &source, &length);
+		i++;
+	}
+
+	return shared_from_this();
 }
 
-void _Shader::link() {
-	if (mVert == 0 || mFrag == 0) {
-		return;
+std::shared_ptr<_Shader> _Shader::link() {
+
+	mProgram = glCreateProgram();
+	
+	for (int i = 0; i < 3; i++) {
+		if (mShaders[i] == GL_NONE) continue;
+
+		glCompileShader(mShaders[i]);
+		if (!mCheckShader(mShaders[i])) continue;
+
+		glAttachShader(mProgram, mShaders[i]);
 	}
 
-	glCompileShader(mVert);
-	if (!mCheckShader(mVert)) {
-        std::cout << "Vertex Shader broke" << std::endl;
-	}
-
-	glCompileShader(mFrag);
-	if (!mCheckShader(mFrag)) {
-       std::cout << "Fragment Shader broke" << std::endl;
-	}
-
-	program = glCreateProgram();
-
-	glAttachShader(program, mVert);
-	glAttachShader(program, mFrag);
-
-	glLinkProgram(program);
-
-	glDeleteShader(mVert);
-	glDeleteShader(mFrag);
+	glLinkProgram(mProgram);
+	return shared_from_this();
 }
 
-void _Shader::use() {
-	glUseProgram(program);
+std::shared_ptr<_Shader> _Shader::use() {
+	glUseProgram(mProgram);
+	return shared_from_this();
+}
+
+std::shared_ptr<_Shader> _Shader::unUse() {
+	glUseProgram(0);
+	return shared_from_this();
+}
+
+void _Shader::addAttribute(const std::string& attribute) {
+}
+
+void _Shader::addUniform(const std::string& uniform) {
 }
 
 bool _Shader::mCheckShader(GLuint uid) {
@@ -95,9 +133,8 @@ bool _Shader::mCheckShader(GLuint uid) {
 	return true;
 }
 
-_Shader::~_Shader() {
-	glDeleteProgram(program);
-	glDeleteShader(mVert);
-	glDeleteShader(mFrag);
-
+std::vector<const _Shader::mPreprocessorDefinition*> _Shader::mGetKeys(std::string key) {
+	std::vector<const _Shader::mPreprocessorDefinition*> ret;
+	for (const auto& p : mDefinitions) if (p.key == key) ret.push_back(&p);
+	return ret;
 }
--- src/rendering/shader.h
+++ src/rendering/shader.h
@@ -4,26 +4,50 @@
 
 #include <string>
 #include <memory>
+#include <vector>
+#include <filesystem>
+#include <unordered_map>
 
 namespace muon::rendering {
 
-class _Shader {
+class _Shader : public std::enable_shared_from_this<_Shader> {
 public:
 	_Shader();
 	~_Shader();
 
-	void load(std::string path);
-	void load(std::string path, GLenum type);
+	std::shared_ptr<_Shader> preprocessorDefine(std::string statement);
 
-	GLuint program;
-	void link();
+	std::shared_ptr<_Shader> load(std::filesystem::path path);
+	std::shared_ptr<_Shader> link();
 
-	void use();
+	std::shared_ptr<_Shader> use();
+	std::shared_ptr<_Shader> unUse();
+
+	void addAttribute(const std::string& attribute);
+	void addUniform(const std::string& uniform);
+	GLuint operator[](const std::string& attribute);
+	GLuint operator()(const std::string& uniform);
+
 private:
+	GLuint mShaders[3];
+	GLuint mProgram;
+
+	std::unordered_map<std::string, GLuint> mAttributes;
+	std::unordered_map<std::string, GLuint> mUniformLocations;
 	bool mCheckShader(GLuint uid);
 
-	GLuint mVert;
-	GLuint mFrag;
+private:
+	// preprocessor definitions can be defined by their
+	// start/end index, this is so that text can either be inserted
+	// in their place or for the "type" definition, the program
+	// can count until the next "type" definition per shader
+	struct mPreprocessorDefinition {
+		int start, end;
+		std::string key;
+		std::string def;
+	};
+	std::vector<mPreprocessorDefinition> mDefinitions;
+	std::vector<const _Shader::mPreprocessorDefinition*> mGetKeys(std::string key);
 };
 typedef std::shared_ptr<_Shader> Shader;
 
